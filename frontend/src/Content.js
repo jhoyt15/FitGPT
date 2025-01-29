6059c3c8483c3729fb2569bc5ba2eb9974c0d035
unknown <jacksonhoyt26@gmail.com>
2025-01-29 09:10:27 -0500

    Added skeleton to make ES queries from react

--- frontend/src/Content.js
+++ frontend/src/Content.js
@@ -1,8 +1,24 @@
-const Content = () => {    
+import { useState } from 'react'
+
+const Content = () => {
+    const [response, setResponse] = useState('')
+
+    const queryES = () => {
+        setResponse('Hello World')
+    }
+
     return (
         <main>
-            <p onDoubleClick = {handleClick}>
+            <p>
                 Hello, world!
+            </p>
+            <input type = "search" onKeyDown = {(e) => {
+                if(e.key === "Enter")
+                    queryES()
+            }}>
+            </input>
+            <p>
+                {response}
             </p>
         </main>
     )
