a30b9cbd9a70482bbc210b5503895f39dafe8b1a
unknown <jacksonhoyt26@gmail.com>
2025-01-29 12:58:29 -0500

    Connected react frontend with Flask API

--- frontend/src/Content.js
+++ frontend/src/Content.js
@@ -1,29 +1,35 @@
 import { useState } from 'react'
 
 const Content = () => {
-    const [response, setResponse] = useState('')
-    const [query,setQuery] = useState('')
+    const [response, handleResponse] = useState([]) //ElasticSearch response state
+    const [query,handleQuery] = useState("") //ElasticSearch Query
 
-    const queryES = (query) => {
-        setResponse('Hello World')
-        fetch('/query',{method:"POST",body:JSON.stringify({query:query})})
+    const queryES = () => {
+        fetch('/query',{method:'POST',headers: {"Content-Type": "application/json"},body:JSON.stringify({query:query})})
             .then(response => response.json())
-            .then(data => setResponse(data))
+            .then(data => handleResponse(data.response))
+            .catch((e) => console.log(e))
     }
 
     return (
-        <main>
-            <p>
-                Hello, world!
-            </p>
-            <input type = "search" value={query} onChange={setQuery} onKeyDown = {(e) => {
+        <main> 
+            <input type = "search" value={query} onChange={(e) => handleQuery(e.target.value)} onKeyDown = {(e) => {
                 if(e.key === "Enter")
                     queryES()
             }}>
             </input>
-            <p>
-                {response}
-            </p>
+
+            { response.length != 0 ? (   //Check if response has any items, if it doesn't display "No Results Found"
+                <ul>
+                    { response.map((workout) => 
+                        <li className = "workout" key = {workout.id}>
+                            {workout._source.Title}
+                        </li> 
+                    )}
+                </ul>
+            ) : (
+                <p>No Results Found</p>   
+            )}
         </main>
     )
 }
