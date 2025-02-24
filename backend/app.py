13a9ce1b932cfc5a108cb1a4f0ee001add6fdc47
Akshar Jinandra <aksharjinandra@gmail.com>
2025-02-24 10:39:50 -0500

    removed vercel, fixed issue of sign in not working

--- backend/app.py
+++ backend/app.py
@@ -19,12 +19,13 @@ from models import User, WorkoutHistory
 import os
 
 app = Flask(__name__)
-# Update CORS configuration
 CORS(app, 
-     resources={r"/*": {"origins": ["http://localhost:3000"], 
-                       "supports_credentials": True,
-                       "allow_headers": ["Content-Type", "Authorization"],
-                       "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"]}})
+     resources={r"/*": {
+         "origins": ["http://localhost:3000"],  # Frontend URL
+         "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+         "allow_headers": ["Content-Type", "Authorization"],
+         "supports_credentials": True
+     }})
 
 app.secret_key = os.getenv('FLASK_SECRET_KEY', 'dev')  # Change this in production
 
