a7b25dd53e607af587941bcff0b39adc2142f19e
unknown <jacksonhoyt26@gmail.com>
2025-04-08 14:55:59 -0400

    Added missing file and imports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,11 @@ import React, { useState } from 'react';
 import './App.css';
 import Content from './Content';
 import Chat from './components/Chat'
-
+import Header from './Header'
 import Home from './Home'; // Import the Home component
 import About from './About'; // Import the About component
 import Contact from './Contact'; // Import the Contact component
+import Footer from './Footer';
 
 function App() {
   const [currentSection, setCurrentSection] = useState('home'); // State to track the current section
